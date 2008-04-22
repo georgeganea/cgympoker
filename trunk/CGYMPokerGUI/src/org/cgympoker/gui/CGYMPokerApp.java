7cdd4348cd6f345162cf37454ec0481e108c3189
mihaitarce <mihaitarce@c6168589-d149-0410-8a35-fbb4e477fda4>
2008-04-22 19:07:49 +0000

    New project location.

---
+++ trunk/CGYMPokerGUI/src/org/cgympoker/gui/CGYMPokerApp.java
@@ -0,0 +1,44 @@
+/*
+ * CGYMPokerApp.java
+ */
+
+package org.cgympoker.gui;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class CGYMPokerApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new CGYMPokerView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of CGYMPokerApp
+     */
+    public static CGYMPokerApp getApplication() {
+        return Application.getInstance(CGYMPokerApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(CGYMPokerApp.class, args);
+    }
+}
