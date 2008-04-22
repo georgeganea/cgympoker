4a43af3d0aff31b29b2201b95a9d97039ecbf489
mihaitarce <mihaitarce@c6168589-d149-0410-8a35-fbb4e477fda4>
2008-04-22 19:07:16 +0000

    New project location. (again)

---
+++ CGYMPoker/src/org/cgympoker/Table.java
@@ -0,0 +1,14 @@
+package org.cgympoker;
+
+import java.util.List;
+
+public interface Table {
+	public enum Status { ANTE, DEAL, BLIND, BET, FLOP,
+		BET_FLOP, TURN, BET_TURN, RIVER, BET_RIVER, RESULT }
+	
+	public Status getStatus();
+	public Player getPlayers();
+	public Player getActivePlayer();
+	public Player getDealer();
+	public List<Card> getCards();
+}
---
+++ CGYMPoker/src/org/cgympoker/TableImpl.java
@@ -0,0 +1,60 @@
+package org.cgympoker;
+
+import java.util.List;
+
+public class TableImpl implements Table {
+
+	@Override
+	public Player getActivePlayer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Card> getCards() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Player getDealer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Player getPlayers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Status getStatus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public void reset() {
+		// TODO
+	}
+	
+	public void addPlayer(Player p) {
+		// TODO
+	}
+	
+	public void removePlayer(Player p) {
+		// TODO
+	}
+
+	public void bet(Player p, int amount) {
+		// TODO
+	}
+	
+	public void fold(Player p) {
+		// TODO
+	}
+	
+	private void advanceState() {
+		// TODO
+	}
+}
