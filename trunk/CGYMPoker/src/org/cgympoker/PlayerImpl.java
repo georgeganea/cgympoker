edbbf2d1b75b113d72b2bc2a6ebb858de6bd3ff9
mihaitarce <mihaitarce@c6168589-d149-0410-8a35-fbb4e477fda4>
2008-04-22 19:05:44 +0000

    New project location.

---
+++ trunk/CGYMPoker/src/org/cgympoker/PlayerImpl.java
@@ -0,0 +1,47 @@
+package org.cgympoker;
+
+import java.util.List;
+
+public class PlayerImpl implements Player {
+
+	@Override
+	public List<Card> getCards() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getChipCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getNumberOfCards() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean getState() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getStateOfCards() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	public void flipCards() {
+		// TODO
+	}
+
+}
