9a36bff930a9db4c170be5276f092a91faa0c0d9
mihaitarce <mihaitarce@c6168589-d149-0410-8a35-fbb4e477fda4>
2008-04-22 19:02:19 +0000

    Now in trunk.

---
+++ trunk/CGYMPoker/src/org/cgympoker/LoginImpl.java
@@ -0,0 +1,13 @@
+package org.cgympoker;
+
+public class LoginImpl implements Login{
+
+	@Override
+	public Server login(String user, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
