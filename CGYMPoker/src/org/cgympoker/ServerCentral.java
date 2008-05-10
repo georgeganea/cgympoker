3be044b94e3b9d8f4bf1c05098c28b34282fa9b0
georgeganea <georgeganea@c6168589-d149-0410-8a35-fbb4e477fda4>
2008-05-10 21:16:57 +0000

    rmi working now = we can finally call a remote method yeeei

--- CGYMPoker/src/org/cgympoker/ServerCentral.java
+++ CGYMPoker/src/org/cgympoker/ServerCentral.java
@@ -1,7 +1,5 @@
 package org.cgympoker;
 import java.io.IOException;
-import java.rmi.Naming;
-import java.rmi.Remote;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
