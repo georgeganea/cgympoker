3d344794c4b2e008a9eb506cdb4037963310a798
ioanaverebi <ioanaverebi@c6168589-d149-0410-8a35-fbb4e477fda4>
2008-05-14 17:28:23 +0000

    added a method in class Login for creating a new account

--- CGYMPoker/src/org/cgympoker/LoginImpl.java
+++ CGYMPoker/src/org/cgympoker/LoginImpl.java
@@ -1,16 +1,16 @@
 package org.cgympoker;
 
-import java.util.List;
-
-
-
+import java.rmi.RemoteException;
 
 public class LoginImpl implements Login{
-
 
     public Server login(String user, String pass)  {
         System.out.println("am apelat o metoda de pe server");
         return new ServerImpl();
+    }
+
+    public Server createAccount(String user, String pass, String firstName, String lastName, String eMail) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
    
