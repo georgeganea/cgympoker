be82ea39d479e9b242bd05cb96cb8f0074fe79f0
ioanaverebi <ioanaverebi@c6168589-d149-0410-8a35-fbb4e477fda4>
2008-05-14 17:28:23 +0000

    added a method in class Login for creating a new account

--- trunk/CGYMPoker/src/org/cgympoker/Login.java
+++ trunk/CGYMPoker/src/org/cgympoker/Login.java
@@ -6,5 +6,7 @@ import java.rmi.RemoteException;
 
 public interface Login extends Remote{
 	public Server login(String user,String pass) throws RemoteException;
+        
+        public Server createAccount(String user,String pass,String firstName,String lastName,String eMail) throws RemoteException;
 }
 
