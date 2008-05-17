72ba13200ad0474ac4bd14ac107a35fc0bd212d7
georgeganea <georgeganea@c6168589-d149-0410-8a35-fbb4e477fda4>
2008-05-17 11:44:46 +0000

    rmi working now = we can finally call a remote method yeeei

--- trunk/CGYMPokerGUI/src/org/cgympoker/gui/testrmi/TestRmi.java
+++ trunk/CGYMPokerGUI/src/org/cgympoker/gui/testrmi/TestRmi.java
@@ -21,14 +21,17 @@ public class TestRmi {
                 //if (System.getSecurityManager() == null) {
             //System.setSecurityManager(new SecurityManager());
        // }
-        try {
+         try {
             String name = "CgymPokerLogin";
-            Registry registry = LocateRegistry.getRegistry("127.0.0.1");
+            Registry registry = LocateRegistry.getRegistry("10.11.1.11");
+            System.out.println("gigi "+registry.list()[0]);
             Remote  login = (Remote) registry.lookup(name);
             //login.login();
+            System.out.println("after remote!  "+login.toString());
             Login log = (Login) login;
-            log.login("user.","pass");
-            System.out.println("login to string "+log.toString());
+            System.out.println("  "+log.login("username","password").toString());
+        
+           // System.out.println("login to string "+log.toString());
         } catch (Exception e) {
             System.err.println("CgymPokerLogin exception:");
             e.printStackTrace();
