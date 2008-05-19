9010ba3fd537724ab7e32b1284307ccfa1ba005f
ioanaverebi <ioanaverebi@c6168589-d149-0410-8a35-fbb4e477fda4>
2008-05-19 20:32:02 +0000

    added method getMoney to Player

--- CGYMPoker/src/org/cgympoker/Player.java
+++ CGYMPoker/src/org/cgympoker/Player.java
@@ -3,10 +3,12 @@ package org.cgympoker;
 import java.util.List;
 
 public interface Player {
+
 	public String getName();
 	public int getChipCount();
 	public int getNumberOfCards();
 	public int getStateOfCards();
 	public List<Card> getCards();
 	public boolean getState(); // active, observer
+        public Integer getMoney();
 }
