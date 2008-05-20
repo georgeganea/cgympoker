e3ea868a35d29bb835308be594641411a83e4b26
ioanaverebi <ioanaverebi@c6168589-d149-0410-8a35-fbb4e477fda4>
2008-05-20 14:15:30 +0000

    saved username(and on request the password) when creating a new account refactored classes CGYMPokerView and CGYMPokerCreateAccount

--- CGYMPokerGUI/src/org/cgympoker/gui/CGYMPokerUtil.java
+++ CGYMPokerGUI/src/org/cgympoker/gui/CGYMPokerUtil.java
@@ -5,6 +5,10 @@
 
 package org.cgympoker.gui;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
@@ -18,5 +22,20 @@ public class CGYMPokerUtil {
                   errorMessage,
                   "CGYM error",
                  JOptionPane.ERROR_MESSAGE);
+    }
+     
+     public static void saveUsernamePassword(boolean b, String username, String password) {
+        try{
+            BufferedWriter output = new BufferedWriter(new FileWriter(new File("pass.cgym")));
+            output.write(username);
+            output.newLine();
+            if (b){
+                output.write(password);
+            }
+            output.close();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
