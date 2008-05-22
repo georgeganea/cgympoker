4f32c244544a564679779e3e91c7ad21a19d5dc3
georgeganea <georgeganea@c6168589-d149-0410-8a35-fbb4e477fda4>
2008-05-22 15:25:54 +0000

    creating user files and reading them at login

---
+++ CGYMPokerCommon/src/org/cgympoker/Felix.java
@@ -0,0 +1,13 @@
+package org.cgympoker;
+
+import org.cgympoker.*;
+
+public interface Felix {
+	public void joinTournament(Tournament t);
+	public void leaveTournamnent();
+	public void bet(int amount);
+	public void raise(int amount);
+	public void call();
+	public void check();
+	public void fold();
+}
---
+++ CGYMPokerCommon/src/org/cgympoker/Table.java
@@ -0,0 +1,17 @@
+package org.cgympoker;
+
+import org.cgympoker.*;
+import java.util.List;
+
+public interface Table {
+	public enum Status { ANTE, DEAL, BLIND, BET, FLOP,
+		BET_FLOP, TURN, BET_TURN, RIVER, BET_RIVER, RESULT }
+	
+	public Status getStatus();
+	public List<Player> getPlayers();
+	public Player getActivePlayer();
+	public Player getDealer();
+	public List<Card> getCards();
+        public String getBlinds();
+        public Integer getAveragePot();
+}
