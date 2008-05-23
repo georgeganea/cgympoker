d1c0de7698058eec3f589a52a98be71f7e71af44
ioanaverebi <ioanaverebi@c6168589-d149-0410-8a35-fbb4e477fda4>
2008-05-23 23:47:21 +0000

    added some test classes

--- trunk/CGYMPoker/src/org/cgympoker/common/Server.java
+++ trunk/CGYMPoker/src/org/cgympoker/common/Server.java
@@ -1,20 +1,18 @@
 package org.cgympoker.common;
 
 import java.util.ArrayList;
-import org.cgympoker.common.*;
-import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
 import org.cgympoker.remoteobserver.Publisher;
 
 public interface Server extends Publisher {
 	public List<Tournament> getAllTournaments() throws RemoteException;
-	public List<Tournament> getStartedTournaments()throws RemoteException;
+	public List<Tournament> getStartedTournaments() throws RemoteException;
 	public List<Tournament> getOpenTournaments()throws RemoteException;
 	public List<Player> getPlayers()throws RemoteException;
 	public Felix getFelix()throws RemoteException;
 	public void disconnect()throws RemoteException;
 
-    public void update(ArrayList<Tournament> tournaments) throws RemoteException;
+        public void update(ArrayList<Tournament> tournaments) throws RemoteException;
 }
 
