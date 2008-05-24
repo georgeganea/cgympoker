c51877166787e418a79026005bfd2a42c3197b53
georgeganea <georgeganea@c6168589-d149-0410-8a35-fbb4e477fda4>
2008-05-24 10:55:39 +0000

    added printStackTrace to EMPTY catch block

--- trunk/CGYMPokerGUI/src/org/cgympoker/remoteobserver/BasicPublisher.java
+++ trunk/CGYMPokerGUI/src/org/cgympoker/remoteobserver/BasicPublisher.java
@@ -43,6 +43,7 @@ public class BasicPublisher implements Publisher {
         deadSubs.addElement(s);// must be dead 
       } 
       catch (java.rmi.RemoteException re) { 
+          re.printStackTrace();
         /*might recover?*/ 
       } 
     } 
