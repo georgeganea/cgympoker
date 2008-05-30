5a1c214daf143b7d630990a4e58867423fb0043d
ioanaverebi <ioanaverebi@c6168589-d149-0410-8a35-fbb4e477fda4>
2008-05-30 10:27:12 +0000

    updated the client

--- trunk/CGYMPokerGUI/src/org/cgympoker/common/Login.java
+++ trunk/CGYMPokerGUI/src/org/cgympoker/common/Login.java
@@ -6,7 +6,7 @@ import java.rmi.RemoteException;
 
 public interface Login extends Remote{
 	public Server login(String user,String pass) throws RemoteException;
-        
+        public ServerCentral loginAdmin(String user,String pass) throws RemoteException;
         public Server createAccount(String user,String pass,String firstName,String lastName,String eMail) throws RemoteException;
 }
 
