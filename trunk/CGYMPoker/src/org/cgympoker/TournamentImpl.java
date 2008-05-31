c471df49c4cfb34eb861d0780cf64843c1587d8e
ioanaverebi <ioanaverebi@c6168589-d149-0410-8a35-fbb4e477fda4>
2008-05-31 18:58:19 +0000

    removed timer and now tournaments can be created only by the admin

--- trunk/CGYMPoker/src/org/cgympoker/TournamentImpl.java
+++ trunk/CGYMPoker/src/org/cgympoker/TournamentImpl.java
@@ -1,6 +1,5 @@
 package org.cgympoker;
 
-import java.io.Serializable;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
@@ -16,88 +15,86 @@ import java.util.List;
 
 public class TournamentImpl implements Tournament {
 
-        private String ID;
-        private String status;
-        private Date startTime;
-        private Date stopTime;
-        private ArrayList<Table> tablesList = new ArrayList<Table>();
-        
-        public TournamentImpl(String ID,Date startDate,Date stopDate){
-               this.ID = ID;
-               this.status = "WAITING TO START";
-               this.startTime = startDate;
-               this.stopTime = stopDate;
+    private String ID;
+    private String status;
+    private Date startTime;
+    private Date stopTime;
+    private ArrayList<Table> tablesList = new ArrayList<Table>();
+
+    public TournamentImpl(String ID, Date startDate, Date stopDate) {
+        this.ID = ID;
+        this.status = "WAITING TO START";
+        this.startTime = startDate;
+        this.stopTime = stopDate;
         try {
-            UnicastRemoteObject.exportObject((Remote) this,0);
+            UnicastRemoteObject.exportObject((Remote) this, 0);
         } catch (RemoteException ex) {
             Logger.getLogger(TournamentImpl.class.getName()).log(Level.SEVERE, null, ex);
         }
-        }
-        
-	@Override
-	public List<Player> getActivePlayers() throws RemoteException{
-		// TODO Auto-generated method stub
-		return null;
-	}
+    }
 
-	@Override
-	public List<Player> getEliminatedPlayers() throws RemoteException{
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public List<Player> getActivePlayers() throws RemoteException {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public List<Player> getPlayers() throws RemoteException{
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public List<Player> getEliminatedPlayers() throws RemoteException {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public Date getStartTime() throws RemoteException{
-            return startTime;
-	}
+    @Override
+    public List<Player> getPlayers() throws RemoteException {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public Date getStopTime() throws RemoteException{
-            return stopTime;
-	}
+    @Override
+    public Date getStartTime() throws RemoteException {
+        return startTime;
+    }
 
-	@Override
-	public List<Table> getTables() throws RemoteException{
-            
-            return tablesList;
-	}
-        
-        public void addTable(Table table)throws RemoteException{
-           tablesList.add(table);
-        }
-	
-	
-	public void start() throws RemoteException{
-		// TODO porneste turneul
+    @Override
+    public Date getStopTime() throws RemoteException {
+        return stopTime;
+    }
+
+    @Override
+    public List<Table> getTables() throws RemoteException {
+
+        return tablesList;
+    }
+
+    public void addTable(Table table) throws RemoteException {
+        tablesList.add(table);
+    }
+
+    public void start() throws RemoteException {
+    // TODO porneste turneul
 		/*
-		 * 1. creeaza mesele de joc
-		 * 2. repartizeaza jucatorii
-		 * 3. le da bani
-		 * 4. reseteaza mesele
-		 */
-	}
-	
-	public void stop() throws RemoteException{
-		// TODO
-	}
+     * 1. creeaza mesele de joc
+     * 2. repartizeaza jucatorii
+     * 3. le da bani
+     * 4. reseteaza mesele
+     */
+    }
 
-    public String getID() throws RemoteException{
-       //TO DO returneaza un identificator al turneului
+    public void stop() throws RemoteException {
+    // TODO
+    }
+
+    public String getID() throws RemoteException {
+        //TO DO returneaza un identificator al turneului
         return ID;
     }
 
-    public String getStatus() throws RemoteException{
+    public String getStatus() throws RemoteException {
         return "Started";
     }
 
-    public void createTable(Table tableImpl) throws RemoteException{
+    public void createTable(Table tableImpl) throws RemoteException {
         tablesList.add(tableImpl);
     }
-
 }
