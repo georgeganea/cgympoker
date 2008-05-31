51ffae2043435c4e6e75df614426d4229e395e1c
ioanaverebi <ioanaverebi@c6168589-d149-0410-8a35-fbb4e477fda4>
2008-05-31 18:58:19 +0000

    removed timer and now tournaments can be created only by the admin

--- CGYMPoker/src/org/cgympoker/ServerCentral.java
+++ CGYMPoker/src/org/cgympoker/ServerCentral.java
@@ -1,17 +1,12 @@
 package org.cgympoker;
 
-import java.io.BufferedReader;
 import java.rmi.RemoteException;
-import java.util.Collection;
-import java.util.ListIterator;
 import org.cgympoker.common.Felix;
 import org.cgympoker.common.Login;
 import org.cgympoker.common.Player;
 import org.cgympoker.common.Tournament;
 import org.cgympoker.common.Server;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -23,25 +18,18 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.cgympoker.common.ServerCentral;
 import org.cgympoker.common.Table.Status;
+import org.cgympoker.test.CGYMServerTest;
 
-public class ServerCentralImpl implements ServerCentral{
+public class ServerCentral {
     //singleton
-    public static final ServerCentralImpl INSTANCE = new ServerCentralImpl();
+    public static final ServerCentral INSTANCE = new ServerCentral();
 
-    private ServerCentralImpl() {
-        try {
-            UnicastRemoteObject.exportObject(this, 0);
-        } catch (RemoteException ex) {
-            ex.printStackTrace();
-            Logger.getLogger(ServerCentralImpl.class.getName()).log(Level.SEVERE, null, ex);
-        }
+    private ServerCentral() {
     }
-    
     private static ArrayList<Tournament> tournaments = new ArrayList<Tournament>();
     private static ArrayList<Server> servers = new ArrayList<Server>();
-    private static String rootdir="."; //the path where we put the userfiles
+
     public static void addServer(Server serv) {
         servers.add(serv);
     }
@@ -50,17 +38,49 @@ public class ServerCentralImpl implements ServerCentral{
  * For testing purposes only
  */
     private static TableImpl table1;
+    private static Tournament createDummyTournament(){
+        Date date = Calendar.getInstance().getTime();
+        Tournament t = new TournamentImpl("T1", date, date);
+        table1 = new TableImpl(Status.ANTE, "10", new ArrayList<Player>(), new Integer(4));
+        ArrayList<Player> playerList = new ArrayList<Player>();
+        playerList.add(new PlayerImpl("Ioana", 100));
+        playerList.add(new PlayerImpl("George", 30));
+        TableImpl table2 = new TableImpl(Status.BET, "20", playerList, 5);
+        try {
+            t.createTable(table1);
+             t.createTable(table2);
+        } catch (RemoteException ex) {
+            ex.printStackTrace();
+            Logger.getLogger(ServerCentral.class.getName()).log(Level.SEVERE, null, ex);
+        }
+       
+        return t;
+    }
     
-    public void createTournament(String ID, Date startTime, Date stopTime) {
+    public static void createTournament(int maxPlayers, Date startTime, Date stopTime) {
         
-        tournaments.add(new TournamentImpl(ID, startTime, stopTime));
+        tournaments.add(createDummyTournament());
+        //TODO de impl cu ceva timer sa se apeleze start tournament
         Iterator<Server> it = servers.iterator();
         while (it.hasNext()) {
             Server s = it.next();
             try {
                 s.update(tournaments);
             } catch (RemoteException ex) {
-                Logger.getLogger(ServerCentralImpl.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(ServerCentral.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    private static void removeOneTournament() {
+        tournaments.remove(0);
+        Iterator<Server> it = servers.iterator();
+        while (it.hasNext()) {
+            Server s = it.next();
+            try {
+                s.update(tournaments);
+            } catch (RemoteException ex) {
+                Logger.getLogger(ServerCentral.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
     }
@@ -118,7 +138,7 @@ public class ServerCentralImpl implements ServerCentral{
             new ClassFileServer(2001, classPath);
         } catch (IOException ex) {
             ex.printStackTrace();
-            Logger.getLogger(ServerCentralImpl.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(ServerCentral.class.getName()).log(Level.SEVERE, null, ex);
         }
 
         try {
@@ -132,65 +152,20 @@ public class ServerCentralImpl implements ServerCentral{
             registry.rebind(name, stub);
 
             System.out.println("CgymPokerLogin");
+            for (int i = 0; i < 1000; i++) {
+              
+                ServerCentral.createTournament(1, new Date(), new Date());
+                Thread.sleep(2000);
+                ((TableImpl)ServerCentral.tournaments.get(0).getTables().get(0)).reset();
+                ServerCentral.createTournament(1, new Date(), new Date());
+                Thread.sleep(2000);
+                ServerCentral.removeOneTournament();
+                System.out.println("i=" + i);
+            }
+
         } catch (Exception e) {
             System.err.println("ComputeEngine exception:");
             e.printStackTrace();
         }
     }
-    
-    public File getUserFile(String username,String directory){
-        File folder = new File(directory+"/");
-        File[] listOfFiles = folder.listFiles();
-        for (int i = 0; i < listOfFiles.length; i++) {
-            if (listOfFiles[i].isFile()) {
-                if(listOfFiles[i].getName().compareTo(username+".cgym")==0)
-                    return listOfFiles[i];
-            } 
-        }
-        return null;
-
-    }
-    public ArrayList<String> getUserList() {
-        String directory=rootdir;
-        File folder = new File(".");
-        File[] listOfFiles = folder.listFiles();
-        ArrayList<String> templist=new ArrayList<String>();
-        for (int i = 0; i< listOfFiles.length ; i++) {
-            if (listOfFiles[i].isFile()&&(listOfFiles[i].getName().endsWith(".cgym"))) {
-                templist. add(listOfFiles[i].getName());
-            } 
-        }
-        return templist;
-    }
-    
-
-    public void deleteUser(String username){
-        File file=ServerCentralImpl.INSTANCE.getUserFile(username, rootdir);
-        if(file!=null){
-            file.delete();
-        }
-    }
-
-    public ArrayList<String> getUserInfo(String username)throws FileNotFoundException,IOException{
-        ArrayList info=new ArrayList<String>();
-        File file=ServerCentralImpl.INSTANCE.getUserFile(username, rootdir);
-        String temp; 
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(username));
-            info.add(username.replace(".cgym", ""));
-            while((temp=reader.readLine())!=null){
-                info.add(temp);
-            }
-            
-        } catch (FileNotFoundException ex) {
-            ex.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return info;
-    }
-
-/*    public void createTournament(Date startTime, Date stopTime) throws RemoteException {
-        System.out.println("Create tournament");
-    } */
 }
---
+++ CGYMPoker/src/org/cgympoker/test/TableImpl.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.cgympoker.test;
+
+/**
+ *
+ * @author Ioana
+ */
+class TableImpl {
+
+}
