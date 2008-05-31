1bc73b955164d8476f6c6ab9c7fd9b9b96ca597b
ioanaverebi <ioanaverebi@c6168589-d149-0410-8a35-fbb4e477fda4>
2008-05-31 21:50:39 +0000

    player can now join a tounament, the server creates the tables and asigns a table to each player when a tournament is started up next : starting the game, and playing it

--- CGYMPokerAdmin/src/java/org/cgympoker/common/Card.java
+++ CGYMPokerAdmin/src/java/org/cgympoker/common/Card.java
@@ -1,12 +1,15 @@
 package org.cgympoker.common;
 
-public interface Card {
-	public enum Rank { DEUCE, THREE, FOUR, FIVE, SIX,
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Card extends Remote{
+    public enum Rank { DEUCE, THREE, FOUR, FIVE, SIX,
         SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE }
 
     public enum Suit { CLUBS, DIAMONDS, HEARTS, SPADES }
     
-    public Rank getRank();
-    public Suit getSuit();
-    public boolean isFlipped();
+    public Rank getRank() throws RemoteException;
+    public Suit getSuit() throws RemoteException;
+    public boolean isFlipped() throws RemoteException;
 }
--- CGYMPokerAdmin/src/java/org/cgympoker/common/Player.java
+++ CGYMPokerAdmin/src/java/org/cgympoker/common/Player.java
@@ -1,14 +1,16 @@
 package org.cgympoker.common;
 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.List;
 
-public interface Player {
+public interface Player extends Remote{
 
-	public String getName();
-	public int getChipCount();
-	public int getNumberOfCards();
-	public int getStateOfCards();
-	public List<Card> getCards();
-	public boolean getState(); // active, observer
-        public Integer getMoney();
+	public String getName() throws RemoteException;
+	public int getChipCount() throws RemoteException;
+	public int getNumberOfCards() throws RemoteException;
+	public int getStateOfCards() throws RemoteException;
+	public List<Card> getCards() throws RemoteException;
+	public boolean getState() throws RemoteException; // active, observer
+        public Integer getMoney() throws RemoteException;
 }
