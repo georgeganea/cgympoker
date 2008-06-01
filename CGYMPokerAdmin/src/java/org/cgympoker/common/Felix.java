2c59943e76e446a7b943cc7820a25440570fb893
ioanaverebi <ioanaverebi@c6168589-d149-0410-8a35-fbb4e477fda4>
2008-06-01 11:15:58 +0000

    changed tournament interface

--- CGYMPokerAdmin/src/java/org/cgympoker/common/Felix.java
+++ CGYMPokerAdmin/src/java/org/cgympoker/common/Felix.java
@@ -2,6 +2,7 @@ package org.cgympoker.common;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 public interface Felix extends Remote{
 	public void leaveTournamnent() throws RemoteException;
@@ -10,4 +11,7 @@ public interface Felix extends Remote{
 	public void call() throws RemoteException;
 	public void check() throws RemoteException;
 	public void fold() throws RemoteException;
+        public Player getMySelf() throws RemoteException;
+        public ArrayList<Player> getOponents() throws RemoteException;
+        public Table getTable() throws RemoteException;
 }
